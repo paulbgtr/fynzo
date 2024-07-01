47caf73d9630076e34947e9b21c2308fbe9abbbb
paulbg <paulbg@hey.com>
2024-07-01 10:25:25 +0300

    feat: connect routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 
+import AppRouter from "./routes/AppRouter";
+
+import DeveloperController from "./controllers/DeveloperController";
+
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hell");
-});
+const developerController = new DeveloperController();
+const appRouter = new AppRouter(app);
+
+const controllers = [developerController];
+
+appRouter.initializeControllers(controllers);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
