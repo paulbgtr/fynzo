e5464250b5d0204bb66a7843fc46791d2d2977fe
paulbg <paulbg@hey.com>
2024-07-02 10:55:16 +0300

    feat: add db instance

---
+++ backend/database/db.ts
@@ -0,0 +1,14 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
+
+// this config is for local development
+const client = new Client({
+  host: "127.0.0.1",
+  port: 5432,
+  user: "postgres",
+  password: "password",
+  database: "db_name",
+});
+
+await client.connect();
+export const db = drizzle(client);
