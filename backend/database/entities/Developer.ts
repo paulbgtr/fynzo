561c80e6278471495d81f88ba5a21e3c9ad456d0
paulbg <paulbg@hey.com>
2024-07-10 15:05:02 +0300

    wipe

--- backend/database/entities/Developer.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "../db";
-import { developers } from "../schemas/developers";
-
-export class Developer {
-  static async getAll() {
-    return await db.select().from(developers);
-  }
-}
--- backend/database/schemas/developers.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-
-export const developers = pgTable("developers", {
-  id: serial("id").primaryKey(),
-  name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }),
-  password: text("password"),
-});
