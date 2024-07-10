831558f981add037c25f738d7ea0063f7b08110b
paulbg <paulbg@hey.com>
2024-07-10 15:14:41 +0300

    setup react-router

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h2 className="font-bold">hello wrodl</h2>
-    </>
-  );
-}
-
-export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./tailwind.css";
+import Root from "./routes/root/index.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
