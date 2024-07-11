f55adbfd042a3aceb479f2fae8ce5d97fd48c646
paulbg <paulbg@hey.com>
2024-07-11 17:04:48 +0300

    chore: setup basic dashboard

--- frontend/src/routes/user/dashboard/index.tsx
+++ frontend/src/routes/user/dashboard/index.tsx
@@ -1,8 +1,43 @@
+import DashboardLayout from "../../../components/layouts/dashboard-layout";
+import ProjectItems from "../../../components/containers/project-items";
+
+const projects = [
+  {
+    id: 1,
+    title: "Project 1",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+  },
+  {
+    id: 4,
+    title: "Project 4",
+  },
+  {
+    id: 5,
+    title: "Project 5",
+  },
+  {
+    id: 6,
+    title: "Project 6",
+  },
+];
+
 const Dashboard = () => {
   return (
-    <article>
-      <h2>Dashboard</h2>
-    </article>
+    <DashboardLayout>
+      <header className="mb-3 space-y-2">
+        <h2 className="text-5xl font-bold">Hey there!</h2>
+        <p className="text-lg">Here are your projects:</p>
+      </header>
+
+      <ProjectItems projects={projects} />
+    </DashboardLayout>
   );
 };
 
