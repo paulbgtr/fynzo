213fcdfbcea60e0c41b3a8754ea0f491ebbd969a
paulbg <paulbg@hey.com>
2024-07-13 09:27:44 +0300

    chore: setup react query

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,7 +1,12 @@
 import AppRoutes from "../routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App = () => {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <AppRoutes />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
