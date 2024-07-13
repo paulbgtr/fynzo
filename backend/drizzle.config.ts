44aeb34846d584c4bdc466b8cd43b66b4bed4ef1
paulbg <paulbg@hey.com>
2024-07-13 08:23:19 +0300

    feat: setup migrations

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./db/schema.ts",
-  out: "./drizzle",
+  schema: "./src/db/schemas.ts",
+  out: "./src/db/drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+    database: "fynzo",
   },
 });
