6a61f71b282e2b60f5bf988250e192b05e935fa1
paulbg <paulbg@hey.com>
2024-07-18 13:19:07 +0300

    ProjectItem: remove boilerplate text

--- frontend/src/components/ui/project-item/index.tsx
+++ frontend/src/components/ui/project-item/index.tsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 const ProjectItem = ({ name }: { name: string }) => {
   return (
     // todo: add useful link
-    <Link className="transition-opacity duration-300 hover:opacity-80" to="/">
+    <Link
+      className="transition-opacity duration-300 hover:opacity-80"
+      to={`/projects/${name}`}
+    >
       <div className="card bg-primary text-primary-content">
         <div className="card-body">
           <h2 className="card-name">{name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
         </div>
       </div>
     </Link>
