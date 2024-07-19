c5b3dc5f15df035cf07e159aed1d8110222f3564
paulbg <paulbg@hey.com>
2024-07-19 13:34:40 +0300

    chore: test tasklist and task in project view

--- frontend/src/components/ui/project-view/index.tsx
+++ frontend/src/components/ui/project-view/index.tsx
@@ -1,5 +1,6 @@
 import type { Project } from "../../../types/Project";
 import ProjectBadges from "../../containers/project-badges";
+import TaskList from "../../containers/task-list";
 
 /**
  * This component renders a project view on the project page.
@@ -8,10 +9,19 @@ import ProjectBadges from "../../containers/project-badges";
  */
 const ProjectView = ({ project }: { project: Project }) => {
   return (
-    <div>
-      <h2 className="text-3xl font-bold">{project.name}</h2>
-      <ProjectBadges mood={project.mood} status={project.status} />
-    </div>
+    <main>
+      <article className="space-y-4">
+        <header className="space-y-2">
+          <h1 className="text-3xl italic font-black">{project.name}</h1>
+          <ProjectBadges mood={project.mood} status={project.status} />
+        </header>
+
+        <section className="space-y-2">
+          <h2 className="text-2xl font-bold">TODO</h2>
+          <TaskList />
+        </section>
+      </article>
+    </main>
   );
 };
 
