3f384b85a1db173bd63775c3b74a31f8a518b58b
paulbg <paulbg@hey.com>
2024-07-19 12:33:18 +0300

    chore: lead to "id" instead of "projectName" in project-item

--- frontend/src/components/ui/project-item/index.tsx
+++ frontend/src/components/ui/project-item/index.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 
-const ProjectItem = ({ name }: { name: string }) => {
+const ProjectItem = ({ id, name }: { id: string; name: string }) => {
   return (
     // todo: add useful link
     <Link
       className="transition-opacity duration-300 hover:opacity-80"
-      to={`/user/projects/${name}`}
+      to={`/user/projects/${id}`}
     >
       <div className="card bg-primary text-primary-content">
         <div className="card-body">
