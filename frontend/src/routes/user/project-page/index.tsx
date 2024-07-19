17d7889fb2f9bb238bb5cbeef1e8e17326584e40
paulbg <paulbg@hey.com>
2024-07-19 12:33:58 +0300

    feat: dynamic project page

--- frontend/src/routes/user/project-page/index.tsx
+++ frontend/src/routes/user/project-page/index.tsx
@@ -1,14 +1,23 @@
 import DashboardLayout from "../../../components/layouts/dashboard-layout";
+import ProjectView from "../../../components/ui/project-view";
+import { useQuery } from "@tanstack/react-query";
+import { fetchProjectById } from "../../../api/projects";
+import Loading from "../../../components/ui/loading";
 import { useParams } from "react-router-dom";
 
 const ProjectPage = () => {
-  const { projectName } = useParams();
+  const { projectId } = useParams();
 
-  console.log(projectName);
+  const { data, isLoading } = useQuery({
+    queryKey: ["project"],
+    queryFn: () => fetchProjectById(projectId),
+  });
+
+  const [project] = data || [];
 
   return (
     <DashboardLayout>
-      <h1>Project Page</h1>
+      {isLoading ? <Loading /> : <ProjectView project={project} />}
     </DashboardLayout>
   );
 };
